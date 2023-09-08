f0d6a25531b4c8156990bf32ff01c72ed1490b1b
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2023-09-08 12:27:32 +0000

    Act

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -6,7 +6,7 @@ console.log({ resultado });
 
 //----------------------------------------------Function number
 
-const adNumbersArrow = (a: number, b: number):number => {
+const adNumbersArrow = (a: number, b: number): number => {
   return a + b;
 };
 const resultadoArrowFunction: number = adNumbersArrow(4, 5);
@@ -14,30 +14,57 @@ console.log({ resultadoArrowFunction });
 
 //----------------------------------------------Function string
 
-const adNumbersArrow2 = (a: number, b: number):string => {
-  return`${a + b}`;
+const adNumbersArrow2 = (a: number, b: number): string => {
+  return `${a + b}`;
 };
-const resultadoArrowFunction2: string= adNumbersArrow2(4, 5);
+const resultadoArrowFunction2: string = adNumbersArrow2(4, 5);
 console.log({ resultadoArrowFunction2 });
 
 //----------------------------------------------Function con --(firstNumber: number, secondNumber?: number, base: number) -- (argumentoObligatorio, argumentoOpcional?, argumentoFijo).
 
-const multyply = (firstNumber: number, secondNumber?: number, base: number= 2) => {
+const multyply = (
+  firstNumber: number,
+  secondNumber?: number,
+  base: number = 2
+) => {
   return firstNumber * base;
 };
 const resultadomultyply: number = multyply(4);
 console.log({ resultadomultyply });
 
-
 //----------------------------------------------Function con string --(firstNumber: number, secondNumber?: number, base: number) -- (argumentoObligatorio, argumentoOpcional?, argumentoFijo).
 
-const multyply2 = (firstNumber: number, secondNumber?: number, base: number= 2):string => {
+const multyply2 = (
+  firstNumber: number,
+  secondNumber?: number,
+  base: number = 2
+): string => {
   return `${firstNumber * base}`;
 };
 const resultadomultyply2: string = multyply2(4);
 console.log({ resultadomultyply2 });
 
-export {};
+console.warn("------------Planteo de Personaje-------------------");
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+const healdCaracter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+const personaje1: Character = {
+  name: "Strider",
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida ${this.hp}`);
+  },
+};
+
+healdCaracter(personaje1, 10);
+
+personaje1.showHp();
 
 export {};
-
